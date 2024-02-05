fd0faa1bf59a8f6a4e58aee5833b2584d1a287f1
VengeanceIsHis <elijahjpoole@gmail.com>
2024-02-05 13:49:39 -0800

    tjhere

--- models/__init__.py
+++ models/__init__.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
-from typing import TYPE_CHECKING
-if TYPE_CHECKING:
-    from models.engine.file_storage import FileStorage
+from models.engine.file_storage import FileStorage
 storage = FileStorage()
 storage.reload()
