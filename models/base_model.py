abb19ab799328fd47313978753cb63c2ccc687dc
VengeanceIsHis <elijahjpoole@gmail.com>
2024-02-05 14:44:28 -0800

    work on base model

--- models/base_model.py
+++ models/base_model.py
@@ -6,24 +6,7 @@ import sys
 from models.__init__ import storage
 from models.engine.file_storage import FileStorage
 class BaseModel():
-    def __init__(self, *args, **kwargs):
-        tform = "%Y-%m-%dT%H:%M:%S.%f"                    
-        self.is_new_instance = True
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.today()
-        self.updated_at = datetime.today()
-        if len(kwargs) != 0:
-            self.is_new_instance = False
-            for k, v in kwargs.items():
-                if k == "created_at" or k == "updated_at":
-                    self.__dict__[k] = datetime.strptime(v, tform)
-                elif k == "__class__":
-                    continue
-                else:
-                    self.__dict__[k] = v
-        if self.is_new_instance:
-            storage.new(self)
-
+    def __init__(self):
     def __str__(self):
         return "[{}] ({}) {}".format(type(self).__name__, self.id, self.__dict__)
 
