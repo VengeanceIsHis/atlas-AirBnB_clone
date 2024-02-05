dbaba255b6fe9735a002e8219dfc082e1f3ec1fc
VengeanceIsHis <elijahjpoole@gmail.com>
2024-02-05 14:10:03 -0800

    fix

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,10 +1,5 @@
 #!/usr/bin/python3
 import json
-from models.state import State
-from models.city import City
-from models.place import Place
-from models.amenity import Amenity
-from models.review import Review
 from typing import TYPE_CHECKING
 if TYPE_CHECKING:
     from models.base_model import BaseModel
