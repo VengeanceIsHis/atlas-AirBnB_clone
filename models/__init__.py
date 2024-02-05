ac285b041c3739353ba5dec7b2302e88d4788610
VengeanceIsHis <elijahjpoole@gmail.com>
2024-02-05 13:45:51 -0800

    fixed init

--- models/__init__.py
+++ models/__init__.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
-from models.engine.file_storage import FileStorage
+from typing import TYPE_CHECKING
+if TYPE_CHECKING:
+    from models.engine.file_storage import FileStorage
 storage = FileStorage
 storage.reload()
