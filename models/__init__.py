fe15687dd3c94f12dec73fca474084d44e9599a4
VengeanceIsHis <143756595+VengeanceIsHis@users.noreply.github.com>
2024-02-05 22:30:10 -0600

    Big Test

--- models/__init__.py
+++ models/__init__.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 from models.engine.file_storage import FileStorage
-from typing import TYPE_CHECKING
+
 storage = FileStorage()
 storage.reload()
--- models/base_model.py
+++ models/base_model.py
@@ -1,12 +1,26 @@
 #!/usr/bin/python3
 import uuid
 from datetime import datetime
-import unittest
-import sys
-from models.__init__ import storage
-from models.engine.file_storage import FileStorage
+import models
+
 class BaseModel():
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
+        time_format = "%Y-%m-%dT%H:%M:%S.%f"
+        self.id = str(uuid.uuidr())
+        self.created_at = datetime.utcnow()
+        self.updated_at = datetime.utcnow()
+
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == "__class__":
+                    continue
+                elif key == "created_at" or key == "updated_at":
+                    setattr(self, key, datetime.strptime(value, time_format))
+                else:
+                    setattr(self, key, value)
+
+        models.storage.new(self)
+
     def __str__(self):
         return "[{}] ({}) {}".format(type(self).__name__, self.id, self.__dict__)
 
@@ -24,4 +38,24 @@ class BaseModel():
     
 
 if __name__ == '__main__':
-    unittest.main()
+    my_model = BaseModel()
+    my_model.name = "My_First_Model"
+    my_model.my_number = 89
+    print(my_model.id)
+    print(my_model)
+    print(type(my_model.created_at))
+    print("--")
+    my_model_json = my_model.to_dict()
+    print(my_model_json)
+    print("JSON of my_model:")
+    for key in my_model_json.keys():
+        print("\t{}: ({}) - {}".format(key, type(my_model_json[key]), my_model_json[key]))
+
+    print("--")
+    my_new_model = BaseModel(**my_model_json)
+    print(my_new_model.id)
+    print(my_new_model)
+    print(type(my_new_model.created_at))
+
+    print("--")
+    print(my_model is my_new_model)
